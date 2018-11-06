90e8bd8c646f6d6be07341f3316caa8689a23ec5
Alberto Castro <betoestuamigo@gmail.com>
2018-11-06 02:50:27 -0600

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestingNetCore.Models;
+
+namespace TestingNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About(string input)
+        {
+            ViewData["Message"] = "Use this to show some memes.";
+            ViewData["Source"] = input;
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
